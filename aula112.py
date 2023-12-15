fea76ae27a7206d20935fba167ee63aae3cf1f2c
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-15 00:53:08 -0300

    adiction of try and except

--- aula112.py
+++ aula112.py
@@ -3,16 +3,27 @@ import os
 caminho_procura = 'C:\\Users\\NOTE\\Downloads\\ProjetoA3_Modelagem'
 termo_procura = '1'
 
+conta = 0
 for raiz, diretorios, arquivos in os.walk(caminho_procura):
     for arquivo in arquivos:
         if termo_procura in arquivo:
-            caminho_completo = os.path.join(raiz, arquivo)
-            nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
-            tamanho = os.path.getsize(caminho_completo)
+            try:
+                conta += 1
+                caminho_completo = os.path.join(raiz, arquivo)
+                nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                tamanho = os.path.getsize(caminho_completo)
 
-            print()
-            print('I found the archive:', arquivo)
-            print('Caminho:', caminho_completo)
-            print('Nome:', nome_arquivo)
-            print('Extensao:', ext_arquivo)
-            print('Tamanho:', tamanho)
+                print()
+                print('I found the archive:', arquivo)
+                print('Caminho:', caminho_completo)
+                print('Nome:', nome_arquivo)
+                print('Extensao:', ext_arquivo)
+                print('Tamanho:', tamanho)
+            except PermissionError as e:
+                print('Sem permissão')
+            except FileNotFoundError as e:
+                print('não foi localizado nenhum arquivo')
+            except Exception as e:
+                print('Erro inesperado', e)
+print()
+print(f'{conta}arquivo(s) encontrado(s)')
